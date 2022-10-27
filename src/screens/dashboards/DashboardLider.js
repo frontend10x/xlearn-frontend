c7542a205332ba4fb4591649e4a7bcb78c473bb0
SrCarlosNarvaez <cpromultimedia@gmail.com>
2022-10-27 10:33:36 -0500

    actualizacion dashbard codigo y css

--- src/screens/dashboards/DashboardLider.js
+++ src/screens/dashboards/DashboardLider.js
@@ -37,17 +37,18 @@ export const DashboardLider = () => {
   return (
     <div className="dashboard__lider" >
       <HeaderDashboard />
-      <div className="dashboard__lider-banner" >
-        <Image src={banner_cursos} alt="banner" className="dashboard__lider-banner_image" />
+      <div className="dashboard__lider-banner" style={{ backgroundImage: `url(${banner_cursos})` }}>
+        {/* <Image src={banner_cursos} alt="banner" className="dashboard__lider-banner_image" /> */}
         <div className="dashboard__lider-banner_content" >
           <h1>¡Hola {name}!</h1>
-          <p><span>Continua estudiando.</span> Mira la ultima actividad en tus cursos</p>
+          <p><span>Continúa aprendiendo.</span> Mira la última actividad en tus cursos</p>
+           
         </div>
       </div>
 
       <div className="xlrn__dashboard__lider-course__route" >
         <div className="xlrn__dashboard__lider-course__route-selection">
-          <input type="button" value="Mi Ruta" className="xlrn__dashboard__lider-course__route-selection-input" />
+          <input type="button" value="Mi ruta" className="xlrn__dashboard__lider-course__route-selection-input" />
         </div>
 
         <div className="xlrn__dashborad__lider-container-block">
@@ -55,7 +56,7 @@ export const DashboardLider = () => {
               {courseRoute&&
 
                 courseRoute.map((item, index) => (
-                  <div className="xlrn__dashboard__lider-block-content d-flex gap-4 " >
+                  <div className="xlrn__dashboard__lider-block-content d-flex" >
                   
                   <Image src={item.file_path} className="xlrn__dashboard__lider-block-image" />
                   <div className="xlrn__dashboard__lider-block-content-titles" key={index}>
@@ -64,12 +65,12 @@ export const DashboardLider = () => {
                   <div className=" xlrn__dashboard__lider-content-info d-flex gap-2">
                   <h4>Progreso: <span>40%</span></h4> | <h4> Lecciones: 6 </h4>
                   </div>
-                  <button onClick={redirect} className="xlrn__dashboard__lider-block-button" value={item.name} id={item.id}>Ver curso</button>
+                  <button onClick={redirect} className="xlrn__dashboard__lider-block-button" value={item.name} id={item.id}>Ingresar</button>
                 </div> 
             </div>
                 ))
               }
-            {/* <div className="xlrn__dashboard__lider-block-content d-flex gap-4" >
+            {/* <div className="xlrn__dashboard__lider-block-content d-flex 5" >
               <Image src={Image_02} className="xlrn__dashboard__lider-block-image" />
               <div className="xlrn__dashboard__lider-block-content-titles" >
                 <p>Curso B</p>
@@ -84,13 +85,13 @@ export const DashboardLider = () => {
         </div>
       </div>
 
-      <h2 className="dashboard__lider-container-title" >Cursos Recomendados</h2>
 
+      <h2 className="dashboard__lider-container-title" >Cursos recomendados</h2>
       <div className="dashboard__lider-container_courses" >
         {course &&
           course.map((item, index) => (
             <div key={index} className="dashboard__lider-container_courses-card" >
-              <Image src={item.image} className="w-75" />
+              <Image src={item.image} className="img-recomendation-xln" />
               <div className="dashboard__lider-container_courses-card-content" >
                 <div className="dashboard__lider-container_courses-card-content-body" >
                   <div className="d-flex justify-content-around" >
@@ -106,13 +107,13 @@ export const DashboardLider = () => {
           ))
         }
       </div>
-      <div className="dashboar__lider-construction-container" >
+      {/* <div className="dashboar__lider-construction-container" >
         <div className="dashboar__lider-construction-content" >
           <h1>Hola bienvenido a <span><Image src={XlearnLogo} className="logo_build" /></span> , estamos en construccion para mostrar el progreso en tus cursos</h1>
           <p>Espera nuestro gran lanzamiento</p>
           <Image src={construccion} className="construccion__image" />
         </div>
-      </div>
+      </div> */}
 
       <div className="dashboard__lider-container-help" >
         <div className="dashboard__lider-container-info">
