edcea9964d58af387115a4bb24bf5380f8b00e92
Thomas Velasquez <thomasvelasquez66@gmail.com>
2023-01-12 12:51:42 -0400

    Diseño de perfil e implementacion de progreso del usuario en carrusel de ruta

--- src/screens/dashboards/Perfil.js
+++ src/screens/dashboards/Perfil.js
@@ -4,6 +4,8 @@ import { useSelector } from 'react-redux'
 import { HeaderDashboard } from '../../componentes/dashboards/HeaderDashboard'
 import { getUserCourseById } from '../../services/services'
 import '../../assets/css/screens/dashboards/StylePerfil.css';
+import { imagenUser } from '../../assets/img'
+import { Footer } from '../../componentes/Footer'
 
 export const Perfil = () => {
 
@@ -13,6 +15,7 @@ export const Perfil = () => {
   useEffect(() => {
     async function getCourseRoute() {
       const data = await getUserCourseById(token, id);
+      console.log(data,'valores');
       setRouteCourses(data.response._embedded.courses)
     }
 
@@ -23,7 +26,10 @@ export const Perfil = () => {
     <div className='profile__section' >
       <HeaderDashboard />
       <Container className='d-flex' >
-        <Col md={3} >HOLA</Col>
+        <Col md={3} >
+          <Image src={imagenUser} className="mt-5" />
+          <h3 className='mt-5' >{name}</h3>
+        </Col>
         <Col md={6} >
           <div className='d-flex flex-column mt-5 mb-5 ' >
             <h2 className='fw-bold' >Mi Perfil</h2>
@@ -32,25 +38,28 @@ export const Perfil = () => {
 
           <div>
             <h2 className='fw-bold'>Cursos en ruta</h2>
-            <div className='card__container' >
-              {routeCourses &&
-                routeCourses.map((item, index) => (
 
-                  <div key={index} className='card__body' >
-                    <Image src={item.file_path} className='card__image' />
-                    <div className='card__content' >
-                      <h5 className='card__title' >
-                        {item.name}
-                      </h5>
+            <Container className='' >
+              <div className='card__container' >
+                {routeCourses &&
+                  routeCourses.map((item, index) => (
+                    <div key={index} className='card__body' >
+                      <Image src={item.file_path} className='card__image' />
+                      <div className='card__content w-100' >
+                        <h5 className='card__title' >{item.name}</h5>
+                        <p>Lessons: {item['lessons:amount']}</p>
+                      </div>
                     </div>
-                  </div>
-                )
-                )
-              }
-            </div>
+                  )
+                  )
+                }
+              </div>
+            </Container>
+
           </div>
         </Col>
       </Container>
+      <Footer />
     </div>
   )
 }
