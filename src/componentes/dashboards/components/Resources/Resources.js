d357906d2220c7fdbbc2d0141590ee78965500df
Jair Zea Paez <jairzeapaez@MacBook-Air-de-Jair.local>
2023-02-14 18:22:18 -0500

    bugfix: resources title

--- src/componentes/dashboards/components/Resources/Resources.js
+++ src/componentes/dashboards/components/Resources/Resources.js
@@ -3,9 +3,10 @@ import { Image } from 'react-bootstrap'
 import { fileZip } from '../../../../assets/img'
 
 export const Resources = ({title, data, download, type = "default"}) => {
+  const renderTitle = () => data.some(e => e?.type === type) && title
   return (
     <>
-      <h3>{title}</h3>
+      <h3>{renderTitle()}</h3>
       {data?.map((item, index) => (
         (type === item?.type ) && (
           <div className="w-50" key={index}>
