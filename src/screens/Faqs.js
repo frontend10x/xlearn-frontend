ff39f94104640f76aef7f0fd2d7c79197b5a2529
SrCarlosNarvaez <cpromultimedia@gmail.com>
2023-07-14 08:04:36 -0500

    solucion conflicto

--- src/screens/Faqs.js
+++ src/screens/Faqs.js
@@ -23,64 +23,6 @@ export const Faqs = () => {
     navigate(`/preguntas/frecuentes/${faqsId}/${faqsName}`);
   };
 
-<<<<<<< HEAD
-    return (
-        <div>
-            <Header />
-            <div style={{ height: "50vh" }} >
-                <section className="faqs-banner" style={{ backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.675)), url("${registerbanner}")` }}>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-6">
-                                <div className="faqs_banner-content">
-                                    <h2 className='faqs-title' >Centro de ayuda</h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </div>
-
-            <div className='container' >
-                <h2 className='faqs-title fw-bold' >Iniciemos con la ayuda</h2>
-                <div className='mb-5' >
-                    {quest &&
-                        quest.map((item, index) => (
-                            <div key={index} className='mb-2'>
-                                <a className='walsheimProBold' style={{ color: "#270bc4", cursor: "pointer", fontSize: "18px" }} onClick={() => detailedFaqs(item.question)} >{item.question}</a>
-                            </div>
-                        ))
-                    }
-                </div>
-                <div className='d-flex justify-content-center justify-content-around' >
-                    <div class="card" style={{ width: "299px", height: "320px", borderColor: "#002333" }}>
-                        <div class="card-body">
-                            <img src={cuposIcon} className='img-fluid mt-3' />
-                            <div className='mt-5' >
-                                <h5 class="walsheimProBold" style={{ color: "#002333" }} >Consultar profesor</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card" style={{ width: "299px", height: "320px", borderColor: "#002333" }}>
-                        <div class="card-body">
-                            <img src={cuposIcon} className='img-fluid mt-3' />
-                            <div className='mt-5' >
-                                <h5 class="walsheimProBold" style={{ color: "#002333" }} >Consultar en comunidad</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card" style={{ width: "299px", height: "320px", borderColor: "#002333" }}>
-                        <div class="card-body">
-                            <img src={cuposIcon} className='img-fluid mt-3' />
-                            <div className='mt-5'>
-                            <h5 class="walsheimProBold" style={{ color: "#002333" }} >Danos un toque</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-                    </div>
-=======
   return (
     <div>
       <Header />
@@ -96,7 +38,6 @@ export const Faqs = () => {
               <div className="col-md-6">
                 <div className="faqs_banner-content">
                   <h2 className="faqs-title">Centro de ayuda</h2>
->>>>>>> dev
                 </div>
               </div>
             </div>
