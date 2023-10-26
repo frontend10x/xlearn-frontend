f275538e2113528e5963ce41591bb5d9cafc3f1e
Jair Zea Paez <jairzeapaez@MacBook-Air-de-Jair.local>
2023-10-26 14:08:35 -0500

    route change

--- src/utils/route.js
+++ src/utils/route.js
@@ -1,4 +1,4 @@
-// export const baseURL = 'https://servicios.10xconsultores.org';
+export const baseURL = 'https://servicios.10xconsultores.org';
 
-export const baseURL = 'http://127.0.0.1:8000';
+// export const baseURL = 'http://127.0.0.1:8000';
 
