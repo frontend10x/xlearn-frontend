0d602f74a24fc0a10134faab1b8aff63e2cda369
Thomas Velasquez <thomasvelasquez66@gmail.com>
2023-12-13 17:25:38 -0400

    Feat de prueba

--- src/utils/route.js
+++ src/utils/route.js
@@ -1,4 +1,5 @@
 export const baseURL = 'https://servicios.10xconsultores.org';
+// export const baseURL = 'https://servicios.10xconsultores.org';
 
 // export const baseURL = 'http://127.0.0.1:8000';
 
